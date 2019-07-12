be1b220db94765ee1c7b859422ef5db459f3c874
yasasvy gunturu <guntur.yasasvy@gmail.com>
2019-07-12 19:05:48 +0530

    image_and_text

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import { Image, View, Text } from 'react-native';
+
+class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text style={{fontSize:20}}>{this.props.text}</Text>
+    );
+  }
+}
 
 export default class App extends Component {
   render() {
     return (
-      <Image source={require("./images/pic.jpg")} style={{width: 420, height: 800}}/>
+      <View>
+      <Image source={require("./images/pic.jpg")} style={{width: 420, height: 600}}/>
+      <Blink text='she is gorgeous' />
+      </View>
     );
   }
 }
